278c1ce3f22a088acd1277b81850cc0d07f485fc
Muhammad Bilal <83014017+bilall1@users.noreply.github.com>
2022-03-25 15:20:16 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,49 @@
+const name = document.getElementById('name')
+const pass = document.getElementById('pass')
+const email = document.getElementById('email')
+const cnic = document.getElementById('cnic')
+const age = document.getElementById('age')
+const gender = document.getElementById('gender')
+const adress = document.getElementById('adress')
+const form= document.getElementById('form')
+const errorElement = document.getElementById('error')
+
+
+form.addEventListener('submit' , (e)=>{
+    let message = []
+    
+    let numbers = false;
+    for ( let i=0; i< name.value.length; i++)
+    {
+        
+        if(i>0 && i<name.value.length-1 && name.value[i-1]!=' ' && name.value[i]==' ' && name.value[i+1]!=' ')
+        {
+            numbers = false;
+        }
+        else
+        {
+            if(name.value[i] <'A' ||  name.value[i] >'z')
+            {
+                numbers = true;
+                break;
+            }
+            if(name.value[i] >'Z' &&  name.value[i] <'a')
+            {
+                numbers = true;
+                break;
+            }
+        }
+        
+    }
+    if(numbers == true)
+    {
+        message.push("Invalid Name. Use alphabets only");
+    }
+    
+    if (message.length > 0)
+    {
+        e.preventDefault();
+        errorElement.innerText = message.join(', ')
+    }
+})
+
